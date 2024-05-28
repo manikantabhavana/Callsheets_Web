0e4f3a86d5b51fbdfd7be4ece5c7fb145f87bfed
sandhya454 <104482875+sandhya454@users.noreply.github.com>
2024-05-28 14:25:10 +0530

    commit

--- Callsheets/src/Components/Auth/Registration/Registration.jsx
+++ Callsheets/src/Components/Auth/Registration/Registration.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Registration.css';
 import regImage from '../../../Assets/Images/Registration/RegistrationBg.png'
+import { Icon } from '@iconify/react';
+import ContinueBtn from '../../Shared/ContinueBtn/ContinueBtn';
 
 
 function Registration() {
@@ -8,9 +10,35 @@ function Registration() {
     <>
         <div className='registration-main-cont'>
             <div className='registration-image'>
-                {/* <img src={regImage} alt="regImage" /> */}
+
             </div>
-            <div className='registration-details'></div>
+            <div className='registration-details'>
+                <h1>Personal Information</h1>
+                <form action="">
+                <div className='input'>
+                    <span><Icon icon="ic:baseline-person" className='icon' /></span>
+                    <input type="text" placeholder='Full Name' />
+                </div>
+                <div className='input'>
+                    <span><Icon icon="clarity:email-solid-badged"  className='icon'/></span>
+                    <input type="text" placeholder='E-mail' />
+                </div>
+                <div className='input'>
+                    <span><Icon icon="ph:gender-intersex-light" className='icon' /></span>
+                    <input type="text" placeholder='Gender' />
+                </div>
+                <div className='input'>
+                    <span><Icon icon="uis:calender" className='icon' /></span>
+                    <input type="date" placeholder='Date of Birth' />
+                </div>
+                <div className='input'>
+                    <span><Icon icon="mdi:telephone" className='icon' /></span>
+                    <input type="text" placeholder='Mobile Number' />
+                </div>
+                <ContinueBtn/>
+                
+                </form>
+            </div>
         </div>
     </>
   )
