668da8b4bb85f8d293948ac4b9ffcfdba1d95fed
manikantabhavana <manikanta.7828@gmail.com>
2024-05-28 10:43:42 +0530

    new commit on 28-5

--- Callsheets/src/Components/Auth/Login/LoginOtp.jsx
+++ Callsheets/src/Components/Auth/Login/LoginOtp.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {useNavigate} from 'react-router-dom'
 import './LoginOtp.css'
 import OtpInput from 'react-otp-input';
@@ -11,6 +11,7 @@ function LoginOtp() {
 
     const [otp, setOtp] = useState('');
     const [timeLeft,setTimeLeft]=useState(60);
+    const [isMobileDevice, setIsMobileDevice] = useState(false);
     const navigate=useNavigate()
 
     const goToSignup=()=>{
@@ -27,10 +28,21 @@ function LoginOtp() {
         }
       };
 
+      useEffect(() => {
+        const checkIsMobile = () => {
+          setIsMobileDevice(window.innerWidth <= 890);
+        };
+        checkIsMobile();
+        window.addEventListener('resize', checkIsMobile);
+    
+        return () => window.removeEventListener('resize', checkIsMobile);
+      }, []);
+
 
   return (
     <div className='login-otp-main-cont'>
         <div className='login-otp-center-cont'>
+            <div>test</div>
             <h1 className='login-header'>Log In</h1>
             <p className='otp-text'>We have send OTP on your
             mobile number</p>
@@ -41,16 +53,17 @@ function LoginOtp() {
             onChange={setOtp}
             numInputs={4}
             renderSeparator={<span style={{width:'1rem'}}> </span>}
-            renderInput={(props) => <input {...props} />}
+            renderInput={(props) => <input {...props} readOnly={isMobileDevice} />}
           
             />
             <p className='otp-time-expire'>{timeLeft} sec left</p>
             <ContinueBtn/>
             <p className='otp-text-signup'>Don’t have an account? <span onClick={goToSignup}> Sign up!</span></p>
-            <Numpad className='otp-numpad' onKeyPress={handleKeyPress} />
+           
           
 
         </div>
+         <Numpad className='otp-numpad' onKeyPress={handleKeyPress} />
     </div>
   )
 }
