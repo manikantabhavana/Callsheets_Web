e507dc04c2b3672a91e48c6756eabc755e406df3
manikantabhavana <manikanta.7828@gmail.com>
2024-05-31 19:43:07 +0530

    new commit

--- Callsheets/src/Components/Schedules/SchedulesList.jsx
+++ Callsheets/src/Components/Schedules/SchedulesList.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import "./Schedules.css"
+import React,{useState} from 'react';
+import "./SchedulesList.css"
 
 function SchedulesList() {
+  const [schedules,setSchedules]=useState([{time:'11:30',title:'shooting for OG Movie at Ramoji Film City'},
+  {time:'4:30',title:'shooting for HHVM Movie at annapurna Film studios'}
+  ])
   return (
-    <div>SchedulesList</div>
+    <div className='schedule-list-main-cont'>
+        <h2>schedules</h2>
+        <div className='schedule-list'>
+            {
+              schedules.map((schedule)=>(
+                
+                <div className='schedule'>
+                  <div className='schedule-time'>{schedule.time}</div>
+                  <div className='schedule-title'>
+                    <p>{schedule.title}</p>
+                  </div>
+
+                </div>
+                
+
+              ))
+            }
+        </div>
+    </div>
   )
 }
 
